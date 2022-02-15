32c49d39ac12b737b1f6265a5ebcecfa42cf4bde
Shine Thyu Zan <shinethyuzan2010@gmail.com>
2022-02-15 10:37:00 +0630

    jetpack compose testing

---
+++ app/src/main/java/com/example/zzzcovid_jetpackcompose/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.zzzcovid_jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.zzzcovid_jetpackcompose.ui.theme.ZzzCovidjetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ZzzCovidjetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                  BoxLayout()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ZzzCovidjetpackComposeTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun BoxLayout(){
+    Box( modifier = Modifier.size(200.dp))
+}
